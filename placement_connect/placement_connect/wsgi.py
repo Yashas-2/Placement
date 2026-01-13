fe307982a01a4e6a07a1810e81e2400cca741630
Yashas HK <yashas2k5@gmail.com>
2026-01-14 03:07:06 +0530

    Add Render build script

--- placement_connect/placement_connect/wsgi.py
+++ placement_connect/placement_connect/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'placement_connect.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'placement_connect.placement_connect.settings')
 
 application = get_wsgi_application()
